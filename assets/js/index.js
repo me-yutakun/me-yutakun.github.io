42381ec8fa0b9b920536d2e3b9e7eebe21005184
Yuta-kun <cosmoscandium@gmail.com>
2021-11-08 21:08:07 +0530

    Initialization done.

---
+++ assets/js/index.js
@@ -0,0 +1,9 @@
+/* Buffer control */
+function AtStart(){
+setTimeout(Preloader(),10000);
+}
+
+function Preloader(){
+document.getElementById("buffer").style.display="none";
+document.getElementById("loadMsg").style.display="none";
+}
