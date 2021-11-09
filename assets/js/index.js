3046ad3d2d0f69e7977897243673ca0298258729
Yuta-kun <cosmoscandium@gmail.com>
2021-11-09 22:39:49 +0530

    Updated on 9-Nov-21

--- assets/js/index.js
+++ assets/js/index.js
@@ -7,3 +7,20 @@ function Preloader(){
 document.getElementById("buffer").style.display="none";
 document.getElementById("loadMsg").style.display="none";
 }
+
+(function() {
+    "use strict";
+
+    const typed = document.querySelector('.typed')
+    if (typed) {
+      let typed_strings = typed.getAttribute('data-typed-items')
+      typed_strings = typed_strings.split(',')
+      new Typed('.typed', {
+        strings: typed_strings,
+        loop: true,
+        typeSpeed: 100,
+        backSpeed: 50,
+        backDelay: 2000
+      });
+    }
+})()
