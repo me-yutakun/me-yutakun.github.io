2b14d9e4c855820b0fbcfd518130620e6f8f592c
Yuta-kun <cosmoscandium@gmail.com>
2022-01-17 15:47:27 +0530

    Hero section done.

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,26 +1,17 @@
 /* Buffer control */
 function AtStart(){
 setTimeout(Preloader(),10000);
+let content = document.querySelector('.typed').getAttribute('data-typed-items').split(',')
+new Typed('.typed', {
+  strings: content,
+  loop: true,
+  typeSpeed: 100,
+  backSpeed: 50,
+  backDelay: 2000
+});
 }
 
 function Preloader(){
 document.getElementById("buffer").style.display="none";
 document.getElementById("loadMsg").style.display="none";
 }
-
-(function() {
-    "use strict";
-
-    const typed = document.querySelector('.typed')
-    if (typed) {
-      let typed_strings = typed.getAttribute('data-typed-items')
-      typed_strings = typed_strings.split(',')
-      new Typed('.typed', {
-        strings: typed_strings,
-        loop: true,
-        typeSpeed: 100,
-        backSpeed: 50,
-        backDelay: 2000
-      });
-    }
-})()
